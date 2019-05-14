68a7689de906578dc5c17a7c341c74eda3e972ed
dlanncpa <drlanning1976@hotmail.com>
2019-05-14 19:54:39 -0400

    Taking a different approach. No longer working with predictions. The app will display stock charts for 3 different stocks.

--- Prediction Code.R
+++ Prediction Code.R
@@ -1,10 +1,17 @@
-library(caret)
-data("mtcars")
+library(quantmod)
+library(lubridate)
 
-head(mtcars)
-str(mtcars)
+startDate<-Sys.Date()-years(2)
+endDate<-Sys.Date()
 
-fit<-lm(mpg ~ wt, mtcars)
-summary(fit)$coef
-summary(fit)$r.squared
+getSymbols(c("AAPL", "FB", "MSFT"), src = "yahoo", from = startDate, to = endDate)
 
+stocks<-as.xts(data.frame(AAPL = AAPL[, "AAPL.Close"], FB = FB[, "FB.Close"], MSFT = MSFT[, "MSFT.Close"]))
+
+head(stocks)
+str(stocks)
+
+candleChart(AAPL, up.col = "black", dn.col = "red", theme = "white", subset = "2019-01-01/")
+addSMA(n = 20, col = "green")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "pink")
